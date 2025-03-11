19c337669730fa620a1854a7cb089a1d38886dc6
Amos Brocco <amos.brocco@gmail.com>
2025-03-11 17:01:58 +0100

    End 11.3

--- S4/main.cpp
+++ S4/main.cpp
@@ -2,8 +2,7 @@
 #include "fraction.h"
 
 void dothings(const Fraction& f) {
-    //f.numerator(2);
-    //f.denominator(3);
+
     f.print();
     //f.numerator(7);
     //f.denominator(3);
@@ -12,7 +11,11 @@ void dothings(const Fraction& f) {
 
 int main()
 {
-    Fraction f1;
+    Fraction f1{2,3};
+    Fraction f2;
+    Fraction f3{7};
+    //f1.numerator(2);
+    //f1.denominator(3);
     dothings(f1);
 
 }
